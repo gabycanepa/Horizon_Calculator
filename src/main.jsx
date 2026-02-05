f8ddee49ab73a86035911e783875f7698d05ffe1
gabycanepa <89221306+gabycanepa@users.noreply.github.com>
2026-02-05 13:02:42 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./styles.css"; // Aquí es donde debe ir el CSS
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
